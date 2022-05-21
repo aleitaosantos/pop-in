5c669182f0a488e27929bc66c4367bf5076cf29a
Alexandre Leitão Santos <alexandre.leitao.santos@gmail.com>
2022-05-21 12:13:05 +0200

    update

--- src/Board.js
+++ src/Board.js
@@ -8,7 +8,6 @@ class Board extends Component {
   static defaultProps = {
     nrows: 5,
     ncols: 5,
-    chanceLightStartsOn: 0.25
   }
   constructor( props ) {
     super( props );
@@ -26,7 +25,7 @@ class Board extends Component {
     for ( let y = 0; y < this.props.nrows; y++ ) {
       let row = [];
       for ( let x = 0; x < this.props.ncols; x++ ) {
-        row.push( Math.random() < this.props.chanceLightStartsOn )
+        row.push( false )
       }
       board.push( row );
     }
